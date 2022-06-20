9a839c6ef6d5fd1acdd9c254ac325dec803a67e6
onoff-jonghan-kim <jonghan.kim@onoff.ne.jp>
2022-06-20 18:00:56 +0900

    생활코딩 react 2022-5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
+// component
+function Header(props){
+  return (
+    <header>
+      <h1><a href='/' onClick={function(event){
+        event.preventDefault();
+        props.onChangeMode();
+      }}>{props.title}</a></h1>
+    </header>
+  );
+}
+function Nav(props){
+  const lis = [];
+  for(let i=0; i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>);
+  }
+  return(
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  );
+}
+function Article(props){
+  return(
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  );
+}
+
+// View App
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'CSS', body:'CSS is ...'},
+    {id:3, title:'JavaScript', body:'javascript is ...'}
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header title="REACT" onChangeMode={function(){
+        alert('I am Header');
+      }}></Header>
+      <Nav topics={topics}></Nav>
+      <Article title="Welcome" body="Hello~!"></Article>
     </div>
   );
 }
